0933cce57e103771c60c98b45c210d2e2f93eec9
Panos Daveas <63146477+panosdaveas@users.noreply.github.com>
2024-09-19 20:21:34 +0000

    Import Material-Tailwind - Working

---
+++ diwi-app/app/Components/Button.js
@@ -0,0 +1,5 @@
+"use client";
+ 
+import { Button } from "@material-tailwind/react";
+ 
+export { Button };
--- diwi-app/tailwind.config.js
+++ diwi-app/tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+// const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from '@material-tailwind/react/utils/withMT';
+
+// /** @type {import('tailwindcss').Config} */
+module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./workspaces/DiWi-DApp/diwi-app/node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./workspaces/DiWi-DApp/diwi-app/node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,4 +20,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+});
