54ac6a8c007b1aafc0dd1091171208bde050c044
panosdaveas <panosntaveas@gmail.com>
2024-10-08 00:21:58 +0300

    Dynamic imports fix #5

--- app/api/encrypt/route.js
+++ app/api/encrypt/route.js
@@ -1,20 +1,20 @@
-import { timelockEncrypt,timelockDecrypt, roundAt } from "tlock-js";
-import { quicknetClient } from "drand-client";
-import { NextResponse } from "next/server";
+// import { timelockEncrypt,timelockDecrypt, roundAt } from "tlock-js";
+// import { quicknetClient } from "drand-client";
+// import { NextResponse } from "next/server";
 
-export async function POST(request) {
-  const { message, dateTime } = await request.json();
-  try {
-    const client = await quicknetClient();
-    const chainInfo = await client.chain().info();
-    const roundNumber = await roundAt(Date.now(), chainInfo);
-    const ciphertext = await timelockDecrypt(
-      roundNumber,
-      Buffer.from(message),
-      client
-    );
-    return NextResponse.json({ ciphertext: ciphertext, dateTime });
-  } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-}
+// export async function POST(request) {
+//   const { message, dateTime } = await request.json();
+//   try {
+//     const client = await quicknetClient();
+//     const chainInfo = await client.chain().info();
+//     const roundNumber = await roundAt(Date.now(), chainInfo);
+//     const ciphertext = await timelockDecrypt(
+//       roundNumber,
+//       Buffer.from(message),
+//       client
+//     );
+//     return NextResponse.json({ ciphertext: ciphertext, dateTime });
+//   } catch (error) {
+//     return NextResponse.json({ error: error.message }, { status: 500 });
+//   }
+// }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 
-const nextConfig = {};
+// const nextConfig = {};
+
+// Path: next.config.js
+const nextConfig = {
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  }
+}
 
 export default nextConfig;
