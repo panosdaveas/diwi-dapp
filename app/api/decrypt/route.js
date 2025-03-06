7d63d7e69f028c382dec3d1f65bdfd7050b867f6
panosdaveas <panosntaveas@gmail.com>
2025-03-06 14:35:47 +0200

    handles update

--- app/api/decrypt/route.js
+++ app/api/decrypt/route.js
@@ -6,8 +6,8 @@ export async function POST(request) {
   const { message } = await request.json();
   try {
     const client = await quicknetClient();
-    const cyphertext = await timelockDecrypt(message, client);
-    return NextResponse.json({ decrypted: cyphertext.toString(), });
+    const ciphertext = await timelockDecrypt(message, client);
+    return NextResponse.json({ decrypted: ciphertext.toString(), });
   } catch (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
--- app/scripts/handles.js
+++ app/scripts/handles.js
@@ -1,16 +1,14 @@
+import EthCrypto from "eth-crypto"
 import { CustomContext } from "@/app/Context/context";
 import { useContext } from "react";
-import { EncryptWithPublicKey } from "../utils/asymmetricEncryption";
-import { timeLockEncryption } from "../utils/timeLockEncrypt";
-import { decryptWithPrivateKey } from "../utils/asymmetricEncryption";
 import { useContractInteraction } from "@/app/scripts/interact";
+const { timelockEncrypt, roundAt } = require("tlock-js");
+const { quicknetClient, testnetQuicknetClient } = require("drand-client");
 
 export function handleScripts() {
   const { data, setData } = useContext(CustomContext);
-  const {
-    sendWillToRecipient,
-    getMessageByUniqueId,
-  } = useContractInteraction();
+  const { sendWillToRecipient, getMessageByUniqueId } =
+    useContractInteraction();
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -31,13 +29,7 @@ export function handleScripts() {
     const encrypted = await EncryptWithPublicKey(publicKey, message);
     try {
       const result = await timeLockEncryption(data.dateTime, encrypted);
-      // setData((prevData) => ({
-      //   ...prevData,
-      //   message: result.ciphertext,
-      //   displayMessage: result.ciphertext,
-      //   tlEncrypted: "true",
-      // }));
-      const emitMessage = await sendWillToRecipient(
+      await sendWillToRecipient(
         uniqueId,
         result.ciphertext
       );
@@ -56,8 +48,16 @@ export function handleScripts() {
     const tlMessage = result.success
       ? result.message
       : "Failed to retrieve message";
-    await handleTimeLockDecryption(tlMessage); // Ensure this completes first
-
+      // if result.success, then continue
+    const tlDecryptedMsg = await handleTimeLockDecryption(tlMessage); // Ensure this completes first
+    // const message = tlDecryption.decryptedMessageString;
+    console.log(tlDecryptedMsg);
+    const decryptedMsg = await handleAsymmetricDecryption(privateKey, tlDecryptedMsg);
+    console.log(decryptedMsg);
+    setData((prevState) => ({
+      ...prevState,
+      displayMessage: decryptedMsg,
+    }));
     // try {
     //   setData((prevData) => {
     //     handleAsymmetricDecryption(privateKey, message); // Pass updated data
@@ -70,51 +70,44 @@ export function handleScripts() {
 
   const handleAsymmetricDecryption = async (privateKey, tlMessage) => {
     try {
-      const message = await decryptWithPrivateKey(
-        privateKey, 
-        tlMessage
-      );
-        // Display the decrypted message without storing it in state
-        // alert(`Decrypted Message: ${message}`);
-      setData((prevData) => ({
-        ...prevData,
-        displayMessage: message,
-        // ciphertext: message,
-      }));
+      await decryptWithPrivateKey(privateKey, tlMessage);
+      // Display the decrypted message without storing it in state
+      // alert(`Decrypted Message: ${message}`);
     } catch (error) {
       console.error("Error in asymmetric decryption:", error);
     }
   };
 
-  const handleTimeLockDecryption = async (tlMessage) => { 
+  const handleTimeLockDecryption = async (tlMessage) => {
     try {
       const response = await fetch("./api/decrypt", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: tlMessage }),
       });
-  
+
       if (!response.ok) {
         const errorData = await response.json();
         throw new Error(errorData.error || "Decryption failed");
-      } 
-  
+      }
+
       const decryptedMessageString = await response.json();
-      
+      // console.log(decryptedMessageString);
+
       // Wait for state update before proceeding to next step
       await new Promise((resolve) => {
         setData((prevState) => {
           const newState = {
             ...prevState,
-            message: decryptedMessageString.decrypted,
-            displayMessage: decryptedMessageString.decrypted,
-            tlEncrypted: "false",
+            // message: decryptedMessageString.decrypted,
+            // displayMessage: decryptedMessageString.decrypted,
+            // tlEncrypted: "false",
           };
           resolve(newState);
-          return newState, decryptedMessageString.decrypted;
+          return newState;
         });
       });
-  
+      return decryptedMessageString.decrypted;
     } catch (error) {
       console.error("Error during decryption:", error);
       setData((prevState) => ({
@@ -122,6 +115,66 @@ export function handleScripts() {
         displayMessage: error.toString(),
       }));
     }
+  };
+
+  const timeLockEncryption = async (dateTime, message) => {
+    // const plaintext = message;
+    const decryptionTime = dateTime.getTime();
+    let client;
+    try {
+      client = await quicknetClient();
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+    const chainInfo = await client.chain().info();
+    const roundNumber = await roundAt(decryptionTime, chainInfo);
+
+    const ciphertext = await timelockEncrypt(
+      roundNumber,
+      Buffer.from(message),
+      client
+    );
+    return {
+      // plaintext,
+      decryptionTime,
+      ciphertext,
+      client,
+    };
+  };
+
+  // Encrypt with public key
+const EncryptWithPublicKey = async (publicKey, message) => {
+    try {
+      const formattedPublicKey = stripHexPrefix(publicKey);
+      const encrypted = await EthCrypto.encryptWithPublicKey(
+        formattedPublicKey, // hex string
+        message // plain text
+      );
+      return EthCrypto.cipher.stringify(encrypted);
+    } catch (error) {
+      console.error("Encryption error:", error);
+      throw error;
+    }
+  }
+
+  // Decrypt with private key
+const decryptWithPrivateKey = async (privateKey, encrypted) => {
+    try {
+      const formattedPrivateKey = stripHexPrefix(privateKey);
+      const decrypted = await EthCrypto.decryptWithPrivateKey(
+        formattedPrivateKey, // hex string
+        encrypted // encrypted text
+      );
+      return decrypted;
+    } catch (error) {
+      console.error("Decryption error:", error);
+      throw error;
+    }
+  }
+
+  // Strip the prefix "0x" from the public key if present
+  const stripHexPrefix = (publicKey) => {
+    return publicKey.startsWith("0x") ? publicKey.slice(2) : publicKey;
   };
 
   return {
