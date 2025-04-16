a10afff9759d359c5d838a72cba0c453c0d73138
panosdaveas <panosntaveas@gmail.com>
2025-04-16 12:40:27 +0300

    environmant variables

--- app/config.js
+++ app/config.js
@@ -1,23 +1,21 @@
 export function Configuration() {
+  //deployed contract address on Avalanche Testnet
   const contractAddress = "0x9fa418894aa4d019d7738a3321ffbc555578e4ba";
-  const publicKey =
-    "0x04125d119281c56781ec193b5868ec6df1ad270215e9d0a154ff7a15380d21170b350c09b060dec8356c952aee05f329a348144d2c03c37ff76a283c2689d8d9db";
-  const privateKey =
-    "c9aadf17ad429b64f83546c751e59e22400038a2d0ec341901fbe7ab446a8615";
   return {
     contractAddress,
   };
 }
 
+// test publicKey
+//0x04125d119281c56781ec193b5868ec6df1ad270215e9d0a154ff7a15380d21170b350c09b060dec8356c952aee05f329a348144d2c03c37ff76a283c2689d8d9db
+
+// test privateKey
+//c9aadf17ad429b64f83546c751e59e22400038a2d0ec341901fbe7ab446a8615
+
 //UserB:
 //0x1c08Ad51b53C9DEaAD8e10C4a208d56a2Bd2cB8d
 
-//implementation address of the contract:
-//
-//0x740b321b8cb98a3b0887c1ed520a2fdddae4f1b6
-
 //proxy contract address:
-//
 //0x9fa418894aa4d019d7738a3321ffbc555578e4ba
 
 //Chain testnet:
